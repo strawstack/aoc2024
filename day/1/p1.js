256df6dd2c8a9a767122975490378832b77f66e4
RichardHayes <rhshadowgreen@gmail.com>
2024-12-01 20:23:43 -0800

    Formatting

--- day/1/p1.js
+++ day/1/p1.js
@@ -1,7 +1,5 @@
 import input from './input.txt';
-import {
-    prototypes
-} from '../../helper.js';
+import { prototypes } from '../../helper.js';
 
 function sol(data) {
     return data.split("\n").dropLast()
