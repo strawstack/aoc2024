b321af73a8168736ce3af5c210dac5a3ae98e6ef
RichardHayes <rhshadowgreen@gmail.com>
2024-12-17 19:12:35 -0800

    partial day 13 and 17 because javascript doesnt support numbers

--- day/13/p2.js
+++ day/13/p2.js
@@ -39,33 +39,35 @@ function solve_p1({ ax, ay, bx, by, x, y }) {
     };
 }
 
-function num(n) {
-    return parseInt(n.toString(), 10);
-}
-
 function solve({ ax, ay, bx, by, x, y }) {
-    const av = {x: num(ax), y: num(ay)};
-    const bv = {x: num(bx), y: num(by)};
-    const pv = {x: num(x), y: num(y)};
+    const av = {x: Number(ax), y: Number(ay)};
+    const bv = {x: Number(bx), y: Number(by)};
+    const pv = {x: Number(x), y: Number(y)};
 
     const t1 = ((y * ax) - (ay * x)) * (ax * bx);
     const t2 = ax * ((ax * by) - (ay * bx));
 
-    const g1 = num(x * t2 - t1);
-    const g2 = num(t2 * ax);
-    const g4 = num(t2 * bx);
-    const n1 = num(t1);
+    const g1 = x * t2 - t1;
+    const g2 = t2 * ax;
+    const g4 = t2 * bx;
+    const n1 = t1;
 
-    const at = g1 / g2;
-    const bt = n1 / g4;
+    const at = Number(g1 * BigInt(10000) / g2) / 10000;
+    const bt = Number(n1 * BigInt(10000) / g4) / 10000;
 
-    console.log(at, bt);
+    const fat = Math.floor(at);
+    const fbt = Math.floor(bt);
 
-    // if ( av.mul(at).add(bv.mul(bt)).sub(pv).mag() < 8 ) {
-    //     return 3 * at + bt;
-    // } else {
-    //     return 0;
-    // }
+    const diff_a = at - fat;
+    const diff_b = bt - fbt;
+    
+    const small = 0.07;
+
+    if ( diff_a < small && diff_b < small ) {
+        return 3 * Math.abs(fat) + Math.abs(fbt);
+    } else {
+        return 0;
+    }
 }
 
 function sol(data) {
@@ -80,14 +82,14 @@ function sol(data) {
             const by = parseInt(result[0][4]);
             const x = parseInt(result[0][5]);
             const y = parseInt(result[0][6]);
-            const big = BigInt(0);// BigInt(10000000000000); // BigInt
+            const big = BigInt(10000000000000); // BigInt
             const big_x = BigInt(x) - big;
             const big_y = BigInt(y) - big;
             return {
-                ax: BigInt(ax), 
-                ay: BigInt(ay), 
-                bx: BigInt(bx), 
-                by: BigInt(by), 
+                ax: BigInt(ax),
+                ay: BigInt(ay),
+                bx: BigInt(bx),
+                by: BigInt(by),
                 x: big_x, 
                 y: big_y
             };
--- day/17/p2.js
+++ day/17/p2.js
@@ -1,5 +1,7 @@
 import input from './input.txt';
-import { prototypes, mod } from '../../helper.js';
+import { prototypes, mod, copy } from '../../helper.js';
+
+const d = false;
 
 function getNext({ ip, prog }) {
     return {
@@ -9,17 +11,21 @@ function getNext({ ip, prog }) {
 }
 
 function makeCombo(reg) {
-    return op => {
+    return (op, debug) => {
         if (op <= 3) {
+            if (debug) return op;
             return op;
     
         } else if (op === 4) {
+            if (debug) return `a[${reg.a}]`;
             return reg.a;
 
         } else if (op === 5) {
+            if (debug) return `b[${reg.b}]`;
             return reg.b;
 
         } else if (op === 6) {
+            if (debug) return `c[${reg.c}]`;
             return reg.c;
 
         } else if (op === 7) {
@@ -48,32 +54,47 @@ function exec({ inst, ip, op, reg }) {
 
     const cmds = {
         adv: op => {
-            reg.a = Math.floor(reg.a / Math.pow(2, combo(op)));
+            const ans = Math.floor(reg.a / Math.pow(2, combo(op)));
+            if (d) console.log(`a[${ans}] = a[${reg.a}] / 2 ** ${combo(op, true)}`);
+            reg.a = ans;
         },
         bxl: op => {
-            reg.b = reg.b ^ op;
+            const ans = reg.b ^ op;
+            if (d) console.log(`b[${ans}] = b[${reg.b}] ^ ${op}`);
+            reg.b = ans;
         },
         bst: op => {
-            reg.b = mod(combo(op), 8);
+            const ans = mod(combo(op), 8);
+            if (d) console.log(`b[${ans}] = ${combo(op, true)} % 8`);
+            reg.b = ans;
         },
         jnz: op => {
+            if (d) console.log(`jnz a[${reg.a}] !== 0`);
             if (reg.a === 0) {
-                nip = ip + 1;    
+                nip = ip + 1;
             } else {
                 nip = op;
             }
         },
         bxc: _ => {
-            reg.b = reg.b ^ reg.c;
+            const ans = reg.b ^ reg.c;
+            if (d) console.log(`b[${ans}] = b[${reg.b}] ^ c[${reg.c}]`);
+            reg.b = ans;
         },
         out: op => {
-            return mod(combo(op), 8);
+            const ans = mod(combo(op), 8);
+            if (d) console.log(`out[${ans}] <- ${combo(op, true)} % 8\n`);
+            return ans;
         },
         bdv: op => {
-            reg.b = Math.floor(reg.a / Math.pow(2, combo(op)));
+            const ans = Math.floor(reg.a / Math.pow(2, combo(op)));
+            if (d) console.log(`b[${ans}] = a[${reg.a}] / 2 ** ${combo(op, true)}`);
+            reg.b = ans;
         },
         cdv: op => {
-            reg.c = Math.floor(reg.a / Math.pow(2, combo(op)));
+            const ans = Math.floor(reg.a / Math.pow(2, combo(op)));
+            if (d) console.log(`c[${ans}] = a[${reg.a}] / 2 ** ${combo(op, true)}`);
+            reg.c = ans;
         }
     };
 
@@ -81,17 +102,10 @@ function exec({ inst, ip, op, reg }) {
     return (lookup[inst] === 'out') ? {out, nip} : {out: null, nip};    
 }
 
-function sol(data) {
-    data = data.trim().split("\n\n");
-    const reg = (() => {
-        const r = data[0].split("\n").map(line => parseInt(line.split(" ")[2], 10));
-        return { a: r[0], b: r[1], c: r[2] };
-    })();
-    const prog = data[1].split(" ")[1].split(",").map(n => parseInt(n, 10));
-    
+function run({reg, prog}) {
     let ip = 0;
     const output = [];
-    while (ip >= 0 && ip < prog.length) {
+    while (ip >= 0 && ip < prog.length - 1) {
         const { inst, op } = getNext({ ip, prog });
         const { out, nip } = exec({ inst, ip, op, reg });
         ip = nip;
@@ -101,5 +115,56 @@ function sol(data) {
     return output.map(n => n.toString()).join(",");
 }
 
+const initial = 1;
+const back_space = 8; 
+
+function valid(a, prog) {
+    if (prog.length === 0) return a;
+    const pv = a;
+    const nv = a * 8;
+    const r = prog[prog.length - 1];
+    for (let i = nv; i < nv + back_space; i++) {
+        let av = i;
+        let b = av % 8;
+        b = b ^ 3;
+        let c = Math.floor(av / Math.pow(2, b));
+        b = b ^ 5;
+        av = Math.floor(av / 8); // 8 = Math.pow(2, 3)
+        b = b ^ c;
+        if (av === pv && (b % 8) === r) {
+            const temp = prog.pop();
+            const res = valid(i, prog);
+            if (res !== false) return res;
+            prog.push(temp);
+        }
+    }
+    return false;
+}
+
+function sol(data) {
+    data = data.trim().split("\n\n");
+    const prog = data[1].split(" ")[1].split(",").map(n => parseInt(n, 10));
+    
+    // Example
+    // b[1] = a[1] % 8
+    // b[2] = b[1] ^ 3
+    // c[0] = a[1] / 2 ** b[2]
+    // b[7] = b[2] ^ 5
+    // a[0] = a[1] / 2 ** 3
+    // b[7] = b[7] ^ c[0]
+    // out[7] <- b[7] % 8
+
+    for (let aa = 0; aa < initial; aa++) {
+        const a = valid(aa, copy(prog));
+        if (a !== false) {
+            console.log(a);
+            console.log(run({ reg: { a, b: 0, c: 0 }, prog }));
+            return;
+        }
+    }
+
+}
+
 prototypes();
-console.log(sol(input));
+// console.log(sol(input));
+sol(input);
